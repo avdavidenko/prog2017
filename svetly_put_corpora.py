17f74f2e22b443b13e28a94f2149bc988b72b15d
avdavidenko <davidenkonastya98@gmail.com>
2017-09-29 21:12:52 +0300

    Update svetly_put_corpora.py

--- svetly_put_corpora.py
+++ svetly_put_corpora.py
@@ -4,47 +4,47 @@ import html
 import os
 import time
 
-#def clean_text (text):
-    #html_content = '<html>....</html>'  # тут какой-то html
-    #regTag = re.compile('<.*?>', re.DOTALL)  # это рег. выражение находит все тэги
-    #regScript = re.compile('<script>.*?</script>', re.DOTALL) # все скрипты
-    #regComment = re.compile('<!--.*?-->', re.DOTALL)  # все комментарии
-    # заменяем ненужные куски на пустую строку
-    #clean_t = regScript.sub("", text)
-    #clean_t = regComment.sub("", clean_t)
-    #clean_t = regTag.sub("", clean_t)
-    #clean_t = html.unescape(clean_t)
-    #return clean_t
+def clean_text (text):
+    html_content = '<html>....</html>'  # тут какой-то html
+    regTag = re.compile('<.*?>', re.DOTALL)  # это рег. выражение находит все тэги
+    regScript = re.compile('<script>.*?</script>', re.DOTALL) # все скрипты
+    regComment = re.compile('<!--.*?-->', re.DOTALL)  # все комментарии
+#     заменяем ненужные куски на пустую строку
+    clean_t = regScript.sub("", text)
+    clean_t = regComment.sub("", clean_t)
+    clean_t = regTag.sub("", clean_t)
+    clean_t = html.unescape(clean_t)
+    return clean_t
 
-def tagging (directory, date, filename, main_text):
+def tagging (date, filename, main_text):
     new_directory = 'Светлый путь\\mystem-plain\\' + date[0][6:10] + '\\' + date[0][3:5] + '\\'
     if os.path.exists(new_directory) == False:
         os.makedirs(new_directory)
     new_directory = new_directory + filename
 #    print (new_directory)
-    print ("mystem.exe " + directory + " " + new_directory)
-    os.system("mystem.exe " + directory + " " + new_directory)
+#    print ("mystem.exe " + directory + " " + new_directory)
+    os.system("mystem.exe " + main_text[0] + " " + new_directory)
     
-def tagging_xml (directory, date, filename, main_text):
+def tagging_xml (date, filename, main_text):
     new_directory = 'Светлый путь\\mystem-xml\\' + date[0][6:10] + '\\' + date[0][3:5] + '\\'
     if os.path.exists(new_directory) == False:
         os.makedirs(new_directory)
     new_directory = new_directory + filename
 #    print (new_directory)
-    print ("mystem.exe " + "--format xml " + directory + " " + new_directory)
-    os.system("mystem.exe " + "--format xml " + directory + " " + new_directory)
+#    print ("mystem.exe " + "--format xml " + directory + " " + new_directory)
+    os.system("mystem.exe " + "--format xml " + main_text[0] + " " + new_directory)
 
 def writing_plain(about_text, title, date, main_text):
-    # print (about_text[0])
+#    print (about_text[0])
     directory = 'Светлый путь\\plain\\' + date[0][6:10] + '\\' + date[0][3:5] + '\\'
-    # print (directory)
+#    print (directory)
     if os.path.exists(directory) == False:
         os.makedirs(directory)
-    # print (title[0])
+#    print (title[0])
     filename =  title[0] + '.txt'
-    # print (filename)
+#    print (filename)
     directory = directory + filename
-    #print (directory)
+#    print (directory)
     with open(directory, 'w', encoding = 'utf-8') as f:
           f.write (about_text)
           f.write ('\n')
@@ -52,8 +52,8 @@ def writing_plain(about_text, title, date, main_text):
               f.write (main_text[0])
           else:
               print ('Main text = 0')
-    tagging (directory, date, filename, main_text)
-    tagging_xml (directory, date, filename, main_text)
+    tagging (date, filename, main_text)
+    tagging_xml (date, filename, main_text)
     return
 
 def csv_info (directory, author, title, date, pageUrl):
@@ -64,7 +64,7 @@ def info (text, pageUrl):
     author = re.findall('<p align="right"><strong>(.*?)</strong>', text)
     if author == []:
         author = ['Noname']
-    # print (author)
+#    print (author)
     title = re.findall('<meta name="title" content="(.*?)" />', text)
     if title[0].count ('"') > 0:
         title[0] = title[0].replace('"', '')
@@ -101,33 +101,39 @@ def info (text, pageUrl):
     if title[0].count ("'") > 0:
         title[0] = title[0].replace ("'", "_")
     title[0] = html.unescape(title[0])
-    # print (title)
-    # topic на странице нет
+#    print (title)
+#    topic на странице нет
     regDate = re.compile('<td valign="top" class="createdate">\n\t\t(.*?)\t</td>', re.DOTALL)
     date = regDate.findall(text)
-    # print (date)
-    # текст статьи находится не вeзде (см., например, 106)
+#    print (date)
+#    текст статьи находится не вeзде (см., например, 106)
     main_text = re.findall ('<p>(.*?)</p>', text)
-    # print (main_text)
+    regMain = re.compile ('<tr><td valign="top">(.*?)<!-- START of joscomment --><!-- END of joscomment --></td>', re.DOTALL)
+    main_text2 = regMain.findall (text)
+    if (len(main_text2) > 0) and (len (main_text[0]) < len (main_text2[0])):
+       main_text[0] = main_text2[0] 
+#    print (main_text[0])
+    main_text[0] = clean_text (main_text[0])
+#    print (main_text[0])
     about_text = '@au ' + author[0] + '\n@ti ' + title[0] + '\n@da ' + date[0][:10] + '\n@url ' + pageUrl
-    # print (about_text)
-    # print ('\n', main_text[0])
-    # article = '@au ' + author[0] + '\n@ti ' + title[0] + '\n@da ' + date[0] + '\n@url ' + pageUrl + '\n' + main_text[0]
-    # print (article)
+#    print (about_text)
+#    print ('\n', main_text[0])
+#    article = '@au ' + author[0] + '\n@ti ' + title[0] + '\n@da ' + date[0] + '\n@url ' + pageUrl + '\n' + main_text[0]
+#    print (article)
     if len(main_text) > 0:
         main_text[0] = html.unescape(main_text[0])
         writing_plain (about_text, title, date, main_text)
     return  
 
-# def tagging (text):
-    # inp = "Светлый путь\plain"
-    # lst = os.listdir(inp)
-    # for fl in lst:
-        # os.system(r"C:\mystem.exe " + inp + os.sep + fl + " mystem-plain" + os.sep + fl)
+#def tagging (text):
+#    inp = "Светлый путь\plain"
+#    lst = os.listdir(inp)
+#    for fl in lst:
+#        os.system(r"C:\mystem.exe " + inp + os.sep + fl + " mystem-plain" + os.sep + fl)
 
 
 def download_page(commonUrl):
-    for i in range(9,12): # 9 919
+    for i in range(13,23): # 9 919
         pageUrl = commonUrl + str(i)
         tryNumber = 0
         succeed = False
@@ -135,9 +141,9 @@ def download_page(commonUrl):
             try:
                 page = urllib.request.urlopen(pageUrl)
                 text = page.read().decode('utf-8')
-                # print (text)
+#                print (text)
                 info(text, pageUrl)
-                # clean_text (text)
+#                clean_text (text)
                 succeed = True
             except urllib.error.HTTPError:
                 tryNumber += 1
@@ -152,5 +158,3 @@ def download_page(commonUrl):
       
 commonUrl = 'http://svetly-put.ru/index.php?option=com_content&view=article&id='
 download_page(commonUrl)
-
-    
