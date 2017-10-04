fe1f8d4c60371a5900b30489640ee4a336508b8d
avdavidenko <davidenkonastya98@gmail.com>
2017-10-04 14:50:47 +0300

    Update svetly_put_corpora.py

--- svetly_put_corpora.py
+++ svetly_put_corpora.py
@@ -1,8 +1,8 @@
 import urllib.request
 import re
-import html
 import os
 import time
+import csv
 
 def clean_text (text):
     html_content = '<html>....</html>'  # тут какой-то html
@@ -23,61 +23,142 @@ def clean_text (text):
     clean_t = regTag.sub("", clean_t)
     clean_t = regSpace.sub(' ', clean_t) 
 #    print (clean_t)
-#    print(html.unescape(clean_t))
+    import html
+    html.unescape(clean_t)
     return clean_t
 
-#def tagging (date, filename, main_text):
-#    new_directory = 'Светлый путь\\mystem-plain\\' + date[0][6:10] + '\\' + date[0][3:5] + '\\'
-#    if os.path.exists(new_directory) == False:
-#        os.makedirs(new_directory)
-#    new_directory = new_directory + filename
-#    print (new_directory)
-#    сложить необработанный текст в файл и дать его майстему на вход. Иначе паймайстем, но это сложно
-#    print ("mystem.exe " + 'Светлый путь\\plain\\' + date[0][6:10] + '\\' + date[0][3:5] + '\\' + filename + " " + new_directory)
-#    os.system("mystem.exe " + filename + " " + new_directory)
+def translit (title):
+    latin = {' ':'_',
+             ',':'',
+             'а':'a',
+             'б':'b',
+             'в':'v',
+             'г':'g',
+             'д':'d',
+             'е':'e',
+             'ё':'yo',
+             'ж':'zh',
+             'з':'z',
+             'и':'i',
+             'й':'y',
+             'к':'k',
+             'л':'l',
+             'м':'m',
+             'н':'n',
+             'о':'o',
+             'п':'p',
+             'р':'r',
+             'с':'s',
+             'т':'t',
+             'у':'u',
+             'ф':'f',
+             'х':'h',
+             'ц':'c',
+             'ч':'ch',
+             'ш':'sh',
+             'щ':'shch',
+             'ъ':'y',
+             'ы':'y',
+             'ь':'',
+             'э':'e',
+             'ю':'yu',
+             'я':'ya',
+             'А':'A',
+             'Б':'B',
+             'В':'V',
+             'Г':'G',
+             'Д':'D',
+             'Е':'E',
+             'Ё':'Yo',
+             'Ж':'Zh',
+             'З':'Z',
+             'И':'I',
+             'Й':'Y',
+             'К':'K',
+             'Л':'L',
+             'М':'M',
+             'Н':'N',
+             'О':'O',
+             'П':'P',
+             'Р':'R',
+             'С':'S',
+             'Т':'T',
+             'У':'U',
+             'Ф':'F',
+             'Х':'H',
+             'Ц':'Ts',
+             'Ч':'Ch',
+             'Ш':'Sh',
+             'Щ':'Shch',
+             'Ъ':'Y',
+             'Ы':'Y',
+             'Ь':'',
+             'Э':'E',
+             'Ю':'Yu',
+             'Я':'Ya'}
+    for i, j in latin.items():
+        title[0] = title[0].replace(i, j)
+#    print(title[0])
     
-#def tagging_xml (date, filename, main_text):
-#    new_directory = 'Светлый путь\\mystem-xml\\' + date[0][6:10] + '\\' + date[0][3:5] + '\\'
-#    if os.path.exists(new_directory) == False:
-#        os.makedirs(new_directory)
-#    new_directory = new_directory + filename
-#    print (new_directory)
-#    print ("mystem.exe " + "--format xml " + directory + " " + new_directory)
-#    os.system("mystem.exe " + "--format xml " + main_text[0] + " " + new_directory)
+def tagging (date, filename, main_text, directory):
+    new_directory = 'Svetly_Put\\mystem-plain\\' + date[0][6:10] + '\\' + date[0][3:5] + '\\'
+    if os.path.exists(new_directory) == False:
+        os.makedirs(new_directory)
+    new_directory = new_directory + filename
+    os.system(r"mystem.exe " + "-dc " + directory + " " + new_directory)
+#    os.system(r"C:\mystem.exe " + inp + os.sep + fl + " mystem-plain" + os.sep + fl)
+   
+def tagging_xml (date, filename, main_text, directory):
+    new_directory = 'Svetly_Put\\mystem-xml\\' + date[0][6:10] + '\\' + date[0][3:5] + '\\'
+    if os.path.exists(new_directory) == False:
+        os.makedirs(new_directory)
+    new_directory = new_directory + filename
+    os.system(r"mystem.exe " + "--format xml -d " + directory + " " + new_directory)
+#    os.system(r"C:\mystem.exe " + inp + os.sep + fl + " mystem-plain" + os.sep + fl)
 
-def writing_plain(about_text, title, date, main_text):
+def writing_plain(about_text, title, date, main_text, author, pageUrl):
 #    print (about_text[0])
-    directory = 'Светлый путь\\plain\\' + date[0][6:10] + '\\' + date[0][3:5] + '\\'
+    directory = 'Svetly_Put\\plain\\' + date[0][6:10] + '\\' + date[0][3:5] + '\\'
 #    print (directory)
     if os.path.exists(directory) == False:
         os.makedirs(directory)
 #    print (title[0])
+    translit(title)
     filename =  title[0] + '.txt'
 #    print (filename)
     directory = directory + filename
 #    print (directory)
     with open(directory, 'w', encoding = 'utf-8') as f:
-          f.write (about_text)
-          f.write ('\n')
           if  len(main_text) > 0:
               f.write (clean_text(main_text[0]))
           else:
               print ('Main text = 0')
-#    tagging (date, filename, main_text)
-#    tagging_xml (date, filename, main_text)
+    csv_file = 'metadata.csv'
+    csv_info (csv_file, directory, author, title, date, pageUrl)
+    tagging (date, filename, main_text, directory)
+    tagging_xml (date, filename, main_text, directory)
+    with open(directory, 'w', encoding = 'utf-8') as f:
+          if  len(main_text) > 0:
+              f.write (about_text)
+              f.write ('\n')
+              f.write (clean_text(main_text[0]))
+          else:
+              print ('Main text = 0')
     return
 
-def csv_info (directory, author, title, date, pageUrl):
-    row = '%s\t%s\t\t\t%s\t%s\tпублицистика\t\t\t\t\tнейтральный\tн-возраст\tн-уровень\tрайонная\t%s\tСветлый путь\t\t%s\tгазета\tРоссия\tИгринский район Удмуртской Республики\tru'
-    print(row(directory, author[0], title[0], date[0][:10], pageUrl, date[0][6:10]))
-    
+def csv_info (csv_file, directory, author, title, date, pageUrl):
+    with open (csv_file, "a", newline = "") as file:
+        writer = csv.writer(file, delimiter = '\t')
+        writer.writerows([[directory, author[0], title[0], date[0][0:10], 'публицистика', '', '', '', '', 'нейтральный', 'н-возраст', 'н-уровень', 'районная', pageUrl, 'Светлый путь', date[0][6:10], 'газета', 'Россия', 'Игринский район Удмуртской Республики', 'ru']])
+
 def info (text, pageUrl):
     author = re.findall('<p align="right"><strong>(.*?)</strong>', text)
     if author == []:
         author = ['Noname']
 #    print (author)
+#    print (text)
     title = re.findall('<meta name="title" content="(.*?)" />', text)
-#    print (title[0])
+#    print (title)
     if title[0].count ('"') > 0:
         title[0] = title[0].replace('"', '')
     if title[0].count ('«') > 0:
@@ -114,9 +195,10 @@ def info (text, pageUrl):
         title[0] = title[0].replace ("'", "_")
     regSpace = re.compile('\s+', re.DOTALL)
     title[0] = regSpace.sub(' ', title[0])
-#    title[0] = html.unescape(title[0])
-    print (pageUrl)
-    print (title[0])
+    import html
+    title[0] = html.unescape(title[0])
+#    print (pageUrl)
+#    print (title[0])
 #    topic на странице нет
     regDate = re.compile('<td valign="top" class="createdate">\n\t\t(.*?)\t</td>', re.DOTALL)
     date = regDate.findall(text)
@@ -150,7 +232,7 @@ def info (text, pageUrl):
     elif main_text[0].count('ӵ') > 0:
         Udmurt = True            
     if len(main_text) > 0 and Udmurt == False:
-        writing_plain (about_text, title, date, main_text)
+        writing_plain (about_text, title, date, main_text, author, pageUrl)
     return  
 
 #def tagging (text):
@@ -161,7 +243,7 @@ def info (text, pageUrl):
 
 
 def download_page(commonUrl):
-    for i in range(9,919): # 9 919
+    for i in range(40,60): # 9 919
         pageUrl = commonUrl + str(i)
         tryNumber = 0
         succeed = False
