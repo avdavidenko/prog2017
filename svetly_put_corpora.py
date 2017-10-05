4e03a0533785314a6274d2cd1eafbdb767ea99ef
avdavidenko <davidenkonastya98@gmail.com>
2017-10-05 12:54:30 +0300

    Update svetly_put_corpora.py

--- svetly_put_corpora.py
+++ svetly_put_corpora.py
@@ -4,20 +4,15 @@ import os
 import time
 import csv
 
-#ДОПИШИ УЖЕ СЧЕТЧИК СЛОВ
-#ПОЧИСТЬ ОТ ПРОБЕЛОВ В НАЧАЛЕ И В КОНЦЕ
-#ПОДЕЛИ НА НОРМАЛЬНЫЕ ФУНКЦИИ
-
 def clean_text (text):
-    html_content = '<html>....</html>'  # тут какой-то html
-    regTag = re.compile('<.*?>', re.DOTALL)  # это рег. выражение находит все тэги
-    regScript = re.compile('<script>.*?</script>', re.DOTALL) # все скрипты
-    regComment = re.compile('<!--.*?-->', re.DOTALL)  # все комментарии
+    html_content = '<html>....</html>'
+    regTag = re.compile('<.*?>', re.DOTALL)
+    regScript = re.compile('<script>.*?</script>', re.DOTALL)
+    regComment = re.compile('<!--.*?-->', re.DOTALL)
     regTitle = re.compile ('<td class="contentheading" width="100%">(.*?)</td>', re.DOTALL)
     regAdm = re.compile ('<span class="small">(.*?)</span>', re.DOTALL)
     regDate = re.compile ('<td valign="top" class="createdate">\n\t\t(.*?)\t</td>', re.DOTALL)
     regSpace = re.compile('\s+', re.DOTALL)
-#     заменяем ненужные куски на пустую строку
     clean_t = text.replace ('&nbsp;', '')
     clean_t = regDate.sub ("", clean_t)
     clean_t = regTitle.sub ("", clean_t)
@@ -26,86 +21,53 @@ def clean_text (text):
     clean_t = regComment.sub("", clean_t)
     clean_t = regTag.sub("", clean_t)
     clean_t = regSpace.sub(' ', clean_t) 
-#    print (clean_t)
     import html
     html.unescape(clean_t)
+    clean_t.strip(clean_t[0])
     return clean_t
 
-def translit (title):
-    latin = {' ':'_',
-             ',':'',
-             'а':'a',
-             'б':'b',
-             'в':'v',
-             'г':'g',
-             'д':'d',
-             'е':'e',
-             'ё':'yo',
-             'ж':'zh',
-             'з':'z',
-             'и':'i',
-             'й':'y',
-             'к':'k',
-             'л':'l',
-             'м':'m',
-             'н':'n',
-             'о':'o',
-             'п':'p',
-             'р':'r',
-             'с':'s',
-             'т':'t',
-             'у':'u',
-             'ф':'f',
-             'х':'h',
-             'ц':'c',
-             'ч':'ch',
-             'ш':'sh',
-             'щ':'shch',
-             'ъ':'y',
-             'ы':'y',
-             'ь':'',
-             'э':'e',
-             'ю':'yu',
-             'я':'ya',
-             'А':'A',
-             'Б':'B',
-             'В':'V',
-             'Г':'G',
-             'Д':'D',
-             'Е':'E',
-             'Ё':'Yo',
-             'Ж':'Zh',
-             'З':'Z',
-             'И':'I',
-             'Й':'Y',
-             'К':'K',
-             'Л':'L',
-             'М':'M',
-             'Н':'N',
-             'О':'O',
-             'П':'P',
-             'Р':'R',
-             'С':'S',
-             'Т':'T',
-             'У':'U',
-             'Ф':'F',
-             'Х':'H',
-             'Ц':'Ts',
-             'Ч':'Ch',
-             'Ш':'Sh',
-             'Щ':'Shch',
-             'Ъ':'Y',
-             'Ы':'Y',
-             'Ь':'',
-             'Э':'E',
-             'Ю':'Yu',
-             'Я':'Ya'}
-    for i, j in latin.items():
-        title[0] = title[0].replace(i, j)
-#    print(title[0])
+def clean_author (author):
+    html_content = '<html>....</html>'
+    regTag = re.compile('<.*?>', re.DOTALL)
+    regScript = re.compile('<script>.*?</script>', re.DOTALL)
+    regComment = re.compile('<!--.*?-->', re.DOTALL)
+    regTitle = re.compile ('<td class="contentheading" width="100%">(.*?)</td>', re.DOTALL)
+    regAdm = re.compile ('<span class="small">(.*?)</span>', re.DOTALL)
+    regDate = re.compile ('<td valign="top" class="createdate">\n\t\t(.*?)\t</td>', re.DOTALL)
+    regSpace = re.compile('\s+', re.DOTALL)
+    author[0] = author[0].replace ('&nbsp;', '')
+    author[0] = regDate.sub ("", author[0])
+    author[0] = regTitle.sub ("", author[0])
+    author[0] = regAdm.sub ("", author[0])
+    author[0] = regScript.sub("", author[0])
+    author[0] = regComment.sub("", author[0])
+    author[0] = regTag.sub("", author[0])
+    author[0] = regSpace.sub(' ', author[0]) 
+    import html
+    html.unescape(author[0])
+    author[0].strip(author[0])
+    return author
 
-def count_text
-    
+def clean_title (title):
+    regSpace = re.compile('\s+', re.DOTALL)
+    title[0] = regSpace.sub(' ', title[0])
+    import html
+    title[0] = html.unescape(title[0])
+    title[0].strip(title[0])
+    return title
+
+def count_text (main_text):
+    words = main_text[0].count (' ') + 1
+    count_file = 'count_file.csv'
+    with open (count_file, "a", newline = "") as file:
+        writer = csv.writer(file, delimiter = '\t')
+        writer.writerows([[words]])
+
+def replace_by_table(title):
+    converted = ""
+    for ch in title[0]:
+        converted += replacement_table.get(ch, ch)
+    title[0] = converted
     
 def tagging (date, filename, main_text, directory):
     new_directory = 'Svetly_Put\\mystem-plain\\' + date[0][6:10] + '\\' + date[0][3:5] + '\\'
@@ -113,7 +75,6 @@ def tagging (date, filename, main_text, directory):
         os.makedirs(new_directory)
     new_directory = new_directory + filename
     os.system(r"mystem.exe " + "-dc " + directory + " " + new_directory)
-#    os.system(r"C:\mystem.exe " + inp + os.sep + fl + " mystem-plain" + os.sep + fl)
    
 def tagging_xml (date, filename, main_text, directory):
     new_directory = 'Svetly_Put\\mystem-xml\\' + date[0][6:10] + '\\' + date[0][3:5] + '\\'
@@ -121,29 +82,25 @@ def tagging_xml (date, filename, main_text, directory):
         os.makedirs(new_directory)
     new_directory = new_directory + filename
     os.system(r"mystem.exe " + "--format xml -d " + directory + " " + new_directory)
-#    os.system(r"C:\mystem.exe " + inp + os.sep + fl + " mystem-plain" + os.sep + fl)
 
 def writing_plain(about_text, title, date, main_text, author, pageUrl):
-#    print (about_text[0])
     directory = 'Svetly_Put\\plain\\' + date[0][6:10] + '\\' + date[0][3:5] + '\\'
-#    print (directory)
     if os.path.exists(directory) == False:
         os.makedirs(directory)
-#    print (title[0])
-    translit(title)
+    replace_by_table(title)
     filename =  title[0] + '.txt'
-#    print (filename)
     directory = directory + filename
-#    print (directory)
     with open(directory, 'w', encoding = 'utf-8') as f:
           if  len(main_text) > 0:
               f.write (clean_text(main_text[0]))
           else:
               print ('Main text = 0')
-    csv_file = 'metadata.csv'
+    csv_file = 'Svetly_Put\\metadata.csv'
     csv_info (csv_file, directory, author, title, date, pageUrl)
     tagging (date, filename, main_text, directory)
     tagging_xml (date, filename, main_text, directory)
+    main_text[0] = clean_text(main_text[0])
+    count_text (main_text)
     with open(directory, 'w', encoding = 'utf-8') as f:
           if  len(main_text) > 0:
               f.write (about_text)
@@ -162,61 +119,16 @@ def info (text, pageUrl):
     author = re.findall('<p align="right"><strong>(.*?)</strong>', text)
     if author == []:
         author = ['Noname']
-#    print (author)
-#    print (text)
+    author = clean_author (author)
+    if author[0].count ('▼') > 0:
+        author[0] = author[0].replace ('▼', '')
     title = re.findall('<meta name="title" content="(.*?)" />', text)
-#    print (title)
-    if title[0].count ('"') > 0:
-        title[0] = title[0].replace('"', '')
-    if title[0].count ('«') > 0:
-        title[0] = title[0].replace('«', '')
-    if title[0].count ('»') > 0:
-        title[0] = title[0].replace('»', '')
-    if title[0].count ('?') > 0:
-        title[0] = title[0].replace ('?', '_')
-    if title[0].count ('*') > 0:
-        title[0] = title[0].replace ('*', '_')
-    if title[0].count ('+') > 0:
-        title[0] = title[0].replace ('+', '_')
-    if title[0].count ('№') > 0:
-        title[0] = title[0].replace ('№', '_')
-    if title[0].count ('?') > 0:
-        title[0] = title[0].replace ('?', '_')
-    if title[0].count ('\t') > 0:
-        title[0] = title[0].replace ('\t', ' ')
-    if title[0].count ('\n') > 0:
-        title[0] = title[0].replace ('\n', ' ')
-    if title[0].count (':') > 0:
-        title[0] = title[0].replace (':', '_')
-    if title[0].count ('|') > 0:
-        title[0] = title[0].replace ('|', '_')
-    if title[0].count ('\\') > 0:
-        title[0] = title[0].replace ('\\', '_')
-    if title[0].count ('>') > 0:
-        title[0] = title[0].replace ('>', '_')                                
-    if title[0].count ('<') > 0:
-        title[0] = title[0].replace ('<', '_')
-    if title[0].count ('/') > 0:
-        title[0] = title[0].replace ('/', '_')
-    if title[0].count ("'") > 0:
-        title[0] = title[0].replace ("'", "_")
-    regSpace = re.compile('\s+', re.DOTALL)
-    title[0] = regSpace.sub(' ', title[0])
-    import html
-    title[0] = html.unescape(title[0])
-#    print (pageUrl)
-#    print (title[0])
-#    topic на странице нет
+    clean_title (title)
     regDate = re.compile('<td valign="top" class="createdate">\n\t\t(.*?)\t</td>', re.DOTALL)
     date = regDate.findall(text)
-#    print (date)
     regMain = re.compile('<table class="contentpaneopen">(.*?)<!-- START of joscomment -->', re.DOTALL)
     main_text = regMain.findall (text)
-#    print (main_text)
-#    main_text[0] = clean_text (main_text[0])
-#    print (main_text[0])
     about_text = '@au ' + author[0] + '\n@ti ' + title[0] + '\n@da ' + date[0][:10] + '\n@url ' + pageUrl
-#    print (about_text)
     Udmurt = False
     if main_text[0].count ('Ӝ') > 0:
         Udmurt = True
@@ -242,29 +154,20 @@ def info (text, pageUrl):
         writing_plain (about_text, title, date, main_text, author, pageUrl)
     return  
 
-#def tagging (text):
-#    inp = "Светлый путь\plain"
-#    lst = os.listdir(inp)
-#    for fl in lst:
-#        os.system(r"C:\mystem.exe " + inp + os.sep + fl + " mystem-plain" + os.sep + fl)
-
-
 def download_page(commonUrl):
     for i in range(9,919): # 9 919
+        print ('article: ', i)
         pageUrl = commonUrl + str(i)
         tryNumber = 0
         succeed = False
-        while succeed == False and tryNumber < 5:
+        while succeed == False and tryNumber < 3:
             try:
                 page = urllib.request.urlopen(pageUrl)
                 text = page.read().decode('utf-8')
-#                print (text)
                 info(text, pageUrl)
-#                clean_text (text)
                 succeed = True
             except urllib.error.HTTPError:
                 tryNumber += 3
-#                time.sleep(2)
             except urllib.error.URLError:
                 tryNumber += 1
                 time.sleep (2)
@@ -275,7 +178,95 @@ def download_page(commonUrl):
 
         if succeed == False:
             print('Error at', pageUrl)
-      
+            
+replacement_table = {' ':'_',
+                     '"':'',
+                     '«':'',
+                     '»':'',
+                     '?':'',
+                     '*':'',
+                     '+':'',
+                     '№':'',
+                     '\t':' ',
+                     '\n':' ',
+                     ':':'',
+                     '|':'',
+                     '\\':'',
+                     '>':'',
+                     '<':'',
+                     '/':'',
+                     "'":'',
+                     '▼':'',
+                     'а':'a',
+                     'б':'b',
+                     'в':'v',
+                     'г':'g',
+                     'д':'d',
+                     'е':'e',
+                     'ё':'yo',
+                     'ж':'zh',
+                     'з':'z',
+                     'и':'i',
+                     'й':'y',
+                     'к':'k',
+                     'л':'l',
+                     'м':'m',
+                     'н':'n',
+                     'о':'o',
+                     'п':'p',
+                     'р':'r',
+                     'с':'s',
+                     'т':'t',
+                     'у':'u',
+                     'ф':'f',
+                     'х':'h',
+                     'ц':'c',
+                     'ч':'ch',
+                     'ш':'sh',
+                     'щ':'shch',
+                     'ъ':'y',
+                     'ы':'y',
+                     'ь':'',
+                     'э':'e',
+                     'ю':'yu',
+                     'я':'ya',
+                     'А':'A',
+                     'Б':'B',
+                     'В':'V',
+                     'Г':'G',
+                     'Д':'D',
+                     'Е':'E',
+                     'Ё':'Yo',
+                     'Ж':'Zh',
+                     'З':'Z',
+                     'И':'I',
+                     'Й':'Y',
+                     'К':'K',
+                     'Л':'L',
+                     'М':'M',
+                     'Н':'N',
+                     'О':'O',
+                     'П':'P',
+                     'Р':'R',
+                     'С':'S',
+                     'Т':'T',
+                     'У':'U',
+                     'Ф':'F',
+                     'Х':'H',
+                     'Ц':'Ts',
+                     'Ч':'Ch',
+                     'Ш':'Sh',
+                     'Щ':'Shch',
+                     'Ъ':'Y',
+                     'Ы':'Y',
+                     'Ь':'',
+                     'Э':'E',
+                     'Ю':'Yu',
+                     'Я':'Ya'}
+
 commonUrl = 'http://svetly-put.ru/index.php?option=com_content&view=article&id='
 download_page(commonUrl)
 print ('The end')
+
+
+    
